52b4e23afd55711da8e63b03e681c9a767f844ed
Mohamed Saleem <mohsal.saleem@gmail.com>
2018-08-23 12:22:15 +0530

    Initial application generated by JHipster-5.2.1

---
+++ src/main/java/com/xedflix/video/config/ApplicationProperties.java
@@ -0,0 +1,14 @@
+package com.xedflix.video.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Properties specific to Xedflix Video Service.
+ * <p>
+ * Properties are configured in the application.yml file.
+ * See {@link io.github.jhipster.config.JHipsterProperties} for a good example.
+ */
+@ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
+public class ApplicationProperties {
+
+}
---
+++ src/main/java/com/xedflix/video/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.xedflix.video.config.audit;
